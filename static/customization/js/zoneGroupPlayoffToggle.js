316dfe44925f49330ca9b5c8d56658a28b232c9f
Михаил Васильев <vmm@rterm.ru>
2014-09-15 14:56:59 +0400

    Выравнивание кода в скрипте зоны/группы/плэй-офф

--- static/customization/js/zoneGroupPlayoffToggle.js
+++ static/customization/js/zoneGroupPlayoffToggle.js
@@ -8,27 +8,27 @@ $(document).ready(function() {
 
         if(activeLabelIndex == 1) {
                 console.log("условие номер 1");
-            $('#chooseZone').prop('disabled', false);
+               $('#chooseZone').prop('disabled', false);
                 console.log("Z vis");
-            $('#chooseGroup').prop('disabled', true);
+              $('#chooseGroup').prop('disabled', true);
                 console.log("G dis");
             $('#choosePlayoff').prop('disabled', true);
                 console.log("P dis");
         }
         if(activeLabelIndex == 2) {
             console.log("условие номер 2");
-            $('#chooseZone').prop('disabled', true);
+               $('#chooseZone').prop('disabled', true);
                 console.log("Z dis");
-            $('#chooseGroup').prop('disabled', false);
+              $('#chooseGroup').prop('disabled', false);
                 console.log("G vis");
             $('#choosePlayoff').prop('disabled', true);
                 console.log("P dis");
         }
         if(activeLabelIndex == 4) {
             console.log("условие номер 3");
-            $('#chooseZone').prop('disabled', true);
+               $('#chooseZone').prop('disabled', true);
                 console.log("Z dis");
-            $('#chooseGroup').prop('disabled', true);
+              $('#chooseGroup').prop('disabled', true);
                 console.log("G dis");
             $('#choosePlayoff').prop('disabled', false);
                 console.log("P vis");
