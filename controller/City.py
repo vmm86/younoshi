2a060485535a42f6742cdc0d815bd2cb3e37c2d4
Михаил Васильев <vmm@rterm.ru>
2014-10-28 12:26:07 +0300

    overflow: auto для блока .modal-open

--- controller/City.py
+++ controller/City.py
@@ -5,7 +5,7 @@ from flask import session, render_template, url_for, request, redirect, flash
 
 from werkzeug.exceptions import default_exceptions, BadRequest, HTTPException, NotFound
 
-from peewee import fn, JOIN_LEFT_OUTER
+from peewee import fn, JOIN_LEFT_OUTER, IntegrityError
 
 from model import City, School
 
@@ -23,49 +23,47 @@ def listCity():
 ### Добавление городов
 @login_required
 def createCity():
-    if request.method == 'POST' and request.form['modify'] == 'create':
-        cityname = request.form['cityName']
+    cityname = request.form['cityName']
 
-        if session['demo']:
-            pass
-        else:
-            City.create(
-                cityName = cityname)
+    if session['demo']:
+        pass
+    else:
+        City.create(
+            cityName = cityname)
 
-        return redirect(
-            url_for('listCity'))
+    city = City.get(cityName = cityname)
+
+    return redirect(
+        url_for('listCity'))
 
 ### Изменение городов
 @login_required
 def updateCity(cityid):
-    if request.method == 'POST' and request.form['modify'] == 'update':
-        cityname = request.form['cityName']
+    cityname = request.form['cityName']
 
-        if session['demo']:
-            pass
-        else:
-            city          = City()
-            city.city_ID  = cityid
-            city.cityName = cityname
-            city.save()
+    if session['demo']:
+        pass
+    else:
+        city          = City()
+        city.city_ID  = cityid
+        city.cityName = cityname
+        city.save()
 
-        return redirect(
-            url_for('listCity'))
+    return redirect(
+        url_for('listCity'))
 
 ### Удаление городов
 @login_required
 def deleteCity(cityid):
-    if request.method == 'POST' and request.form['modify'] == 'delete':
+    if session['demo']:
+        pass
+    else:
+        # Ограничение по внешнему ключу FK_School_City 
+        # не позволяет удалить населённый пункт при наличии связанных с ним спортивных школ.
+        try:
+            City.get(city_ID = cityid).delete_instance()
+        except IntegrityError:
+            flash('Вы не можете удалить этот город, пока в него добавлена хотя бы одна спортивная школа', 'danger')
 
-        if session['demo']:
-            pass
-        else:
-            # Ограничение по внешнему ключу FK_School_City 
-            # не позволяет удалить населённый пункт при наличии связанных с ним спортивных школ.
-            try:
-                City.get(city_ID = cityid).delete_instance()
-            except IntegrityError:
-                flash('Вы не можете удалить этот город, пока в него добавлена хотя бы одна спортивная школа', 'danger')
-
-        return redirect(
-            url_for('listCity'))
+    return redirect(
+        url_for('listCity'))
