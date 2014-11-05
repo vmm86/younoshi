ff16d6216a44188798aede19aea9dc12c973ad8c
Михаил Васильев <vmm@rterm.ru>
2014-11-05 15:08:20 +0300

    Переписана работа с данными для SAS

--- controller/SAS.py
+++ controller/SAS.py
@@ -7,7 +7,7 @@ from flask import session, render_template, url_for, request, redirect, flash
 
 from werkzeug.exceptions import default_exceptions, BadRequest, HTTPException, NotFound
 
-from peewee import RawQuery, fn, JOIN_LEFT_OUTER, DoesNotExist
+from peewee import RawQuery, fn, JOIN_LEFT_OUTER, DoesNotExist, IntegrityError
 
 from model import Season, Age, Stage, GameType, SAS, SAST, GP
 
@@ -16,10 +16,9 @@ from User import login_required
 ## Игровые этапы
 @login_required
 def listSAS(seasonid, ageid):
+    # Список сезонов и возрастов, название текущего сезона и возраста
     listSeason = Season.select().order_by(Season.season_ID.asc())
-
-    listAge = Age.select().order_by(Age.ageName)
-
+    listAge    = Age.select().order_by(Age.ageName)
     try:
         seasonname = Season.get(season_ID = seasonid).seasonName
         agename    = Age.get(age_ID = ageid).ageName
@@ -27,10 +26,28 @@ def listSAS(seasonid, ageid):
         seasonname = None
         agename    = None
 
+    # Список игровых стадий
     listStage    = Stage.select().order_by(Stage.stageType, Stage.stageName)
+    # Список типов соревноавний
     listGameType = GameType.select().order_by(GameType.gameTypeName)
+    # Список игровых этапов с количеством дочерних команд и матчей
     listSAS = (RawQuery(SAS, 
-        'SELECT `SAS`.*, (SELECT COUNT(`SAST2`.`SAS_ID`) FROM `SeasonAgeStageTeam` AS `SAST2` WHERE `SAST2`.`SAS_ID` = `SAS`.`SAS_ID`) AS `countSAST`, (SELECT COUNT(`GP2`.`SAS_ID`) FROM `GameProtocol` AS `GP2` WHERE ((`GP2`.`SAS_ID` = `SAST`.`SAS_ID`) AND (`SAST`.`SAS_ID` = `SAS`.`SAS_ID` )) ) AS `countGP` FROM `SeasonAgeStage` AS `SAS` LEFT JOIN `SeasonAgeStageTeam` AS `SAST` ON (`SAS`.`SAS_ID` = `SAST`.`SAS_ID`) LEFT JOIN `Stage` ON (`SAS`.`stage_ID` = `Stage`.`stage_ID`) WHERE ((`SAS`.`season_ID` = %s) AND (`SAS`.`age_ID` = %s)) GROUP BY `SAS`.`SAS_ID` ORDER BY `SAS`.`SAS_ID` ASC', seasonid, ageid))
+        'SELECT `SAS`.*, (SELECT COUNT(`SAST2`.`SAS_ID`) FROM `SeasonAgeStageTeam` AS `SAST2` WHERE `SAST2`.`SAS_ID` = `SAS`.`SAS_ID`) AS `countSAST`, (SELECT COUNT(`GP2`.`SAS_ID`) FROM `GameProtocol` AS `GP2` WHERE ((`GP2`.`SAS_ID` = `SAST`.`SAS_ID`) AND (`SAST`.`SAS_ID` = `SAS`.`SAS_ID` ))) AS `countGP` FROM `SeasonAgeStage` AS `SAS` LEFT JOIN `SeasonAgeStageTeam` AS `SAST` ON (`SAS`.`SAS_ID` = `SAST`.`SAS_ID`) LEFT JOIN `Stage` ON (`SAS`.`stage_ID` = `Stage`.`stage_ID`) WHERE ((`SAS`.`season_ID` = %s) AND (`SAS`.`age_ID` = %s)) GROUP BY `SAS`.`SAS_ID` ORDER BY `SAS`.`SAS_ID` ASC', seasonid, ageid))
+
+    # Переменные для автозаполнения формы добавления/обновления данных в JS-скрипте шаблона
+    ## Список полей
+    modifyFields = ['stage', 'zoneGroupPlayoffToggle', 'gameType', 'startDate', 'finishDate']
+    ## Список групп радиокнопок
+    modifyRadios = ['zoneGroupPlayoffToggle', 'gameType']
+    ## Список групп радиокнопок
+    modifySelect = ['stage']   
+    ## Заголовки модального окна
+    createHeader = ['"Создать новую игровую стадию"']
+    updateHeader = ['"Изменить игровую стадию для возраста "', agename, '" в сезоне "', seasonname]
+    ## Действия формы
+    createAction = ['"/season/"', seasonid, '"/age/"', ageid, '"/stage/create"']
+    updateAction = ['"/season/"', seasonid, '"/age/"', ageid, '"/stage/"', 'PK_ID', '"/update"']
+    deleteAction = ['"/season/"', seasonid, '"/age/"', ageid, '"/stage/"', 'PK_ID', '"/delete"']
 
     return render_template(
         'SAS.jinja.html', 
@@ -42,82 +59,112 @@ def listSAS(seasonid, ageid):
         agename      = agename,
         listStage    = listStage,
         listGameType = listGameType,
-        listSAS      = listSAS)
+        listSAS      = listSAS,
+        modifyFields = modifyFields,
+        modifyRadios = modifyRadios,
+        modifySelect = modifySelect,
+        createHeader = createHeader,
+        updateHeader = updateHeader,
+        createAction = createAction,
+        updateAction = updateAction,
+        deleteAction = deleteAction)
 
 ### Добавление игровых этапов
 @login_required
 def createSAS(seasonid, ageid):
-    if request.method == 'POST' and request.form['modify'] == 'create':
-        stageid    = request.form['stage']
-        gametype   = request.form['gameType']
-        startdate  = datetime.datetime.strptime(request.form['startDate'],  '%d.%m.%Y').strftime('%Y-%m-%d')
-        finishdate = datetime.datetime.strptime(request.form['finishDate'], '%d.%m.%Y').strftime('%Y-%m-%d')
+    # Получение полей формы из шаблона
+    stageid    = request.form['stage']
+    gametype   = request.form['gameType']
+    try:
+        startdate  = request.form['startDate']
+        finishdate = request.form['finishDate']
+    except DoesNotExist:
+        startdate  = None
+        finishdate = None
 
-        if session['demo']:
-            pass
-        else:
-            # Ограничение по ключу UNIQUE_Season_Age_Stage 
-            # не позволяет добавить повторяющуюся комбинацию сезон/возраст/стадия в игровом этапе.
-            # Например, нельзя добавить второй этап плэй-офф в одном и том же сезоне и возрасте.
-            try:
-                SAS.create(
-                    season_ID   = seasonid, 
-                    age_ID      = ageid, 
-                    stage_ID    = stageid, 
-                    gameType_ID = gametype,
-                    startDate   = startdate,
-                    finishDate  = finishdate)
-            except IntegrityError:
-                flash('Вы не можете добавить ещё один такой же игровой этап в одном и том же сезоне и возрасте', 'danger')
+    if startdate:
+        startdate  = datetime.datetime.strptime(startdate,  '%d.%m.%Y').strftime('%Y-%m-%d')
+    if finishdate:
+        finishdate = datetime.datetime.strptime(finishdate, '%d.%m.%Y').strftime('%Y-%m-%d')
 
-        return redirect(
-            url_for('listSAS',
-                seasonid = seasonid,
-                ageid    = ageid))
+    # Сохранение новой записи в БД
+    if session['demo']:
+        pass
+    else:
+        # Ограничение по ключу UNIQUE_Season_Age_Stage 
+        # не позволяет добавить повторяющуюся комбинацию сезон/возраст/стадия в игровом этапе.
+        # Например, нельзя добавить второй этап плэй-офф в одном и том же сезоне и возрасте.
+        try:
+            SAS.create(
+                season_ID   = seasonid, 
+                age_ID      = ageid, 
+                stage_ID    = stageid, 
+                gameType_ID = gametype,
+                startDate   = startdate,
+                finishDate  = finishdate)
+        except IntegrityError:
+            flash('Вы не можете добавить ещё один такой же игровой этап в одном и том же сезоне и возрасте', 'danger')
+
+    # Редирект на вид list
+    return redirect(
+        url_for('listSAS',
+            seasonid = seasonid,
+            ageid    = ageid))
 
 ### Изменение игровых этапов
 @login_required
 def updateSAS(seasonid, ageid, sasid):
-    if request.method == 'POST' and request.form['modify'] == 'update':
-        stageid    = request.form['stage']
-        gametype   = request.form['gameType']
-        startdate  = datetime.datetime.strptime(request.form['startDate'], '%d.%m.%Y').strftime('%Y-%m-%d')
-        finishdate = datetime.datetime.strptime(request.form['finishDate'], '%d.%m.%Y').strftime('%Y-%m-%d')
+    # Получение полей формы из шаблона
+    stageid    = request.form['stage']
+    gametype   = request.form['gameType']
+    try:
+        startdate  = request.form['startDate']
+        finishdate = request.form['finishDate']
+    except DoesNotExist:
+        startdate  = None
+        finishdate = None
 
-        if session['demo']:
-            pass
-        else:
-            SAS             = SAS()
-            SAS.SAS_ID      = sasid
-            SAS.season_ID   = seasonid
-            SAS.age_ID      = ageid
-            SAS.stage_ID    = stageid
-            SAS.gameType_ID = gametype
-            SAS.startDate   = startdate
-            SAS.finishDate  = finishdate
-            SAS.save()
+    if startdate:
+        startdate  = datetime.datetime.strptime(startdate,  '%d.%m.%Y').strftime('%Y-%m-%d')
+    if finishdate:
+        finishdate = datetime.datetime.strptime(finishdate, '%d.%m.%Y').strftime('%Y-%m-%d')
 
-        return redirect(
-            url_for('listSAS',
-                seasonid = seasonid,
-                ageid    = ageid))
+    # Обновление текущей записи в БД
+    if session['demo']:
+        pass
+    else:
+        sas             = SAS()
+        sas.SAS_ID      = sasid
+        sas.season_ID   = seasonid
+        sas.age_ID      = ageid
+        sas.stage_ID    = stageid
+        sas.gameType_ID = gametype
+        sas.startDate   = startdate
+        sas.finishDate  = finishdate
+        sas.save()
+
+    # Редирект на вид list
+    return redirect(
+        url_for('listSAS',
+            seasonid = seasonid,
+            ageid    = ageid))
 
 ### Удаление игровых этапов
 @login_required
 def deleteSAS(seasonid, ageid, sasid):
-    if request.method == 'POST' and request.form['modify'] == 'delete':
+    # Удаление текущей записи в БД
+    if session['demo']:
+        pass
+    else:
+        # Ограничение по внешнему ключу FK_SAST_SAS 
+        # не позволяет удалить игровой этап при наличии связанных с ним дочерних команд.
+        try:
+            SAS.get(SAS_ID = sasid).delete_instance()
+        except IntegrityError:
+            flash('Вы не можете удалить этот игровой этап, пока в него добавлена хотя бы одна команда', 'danger')
 
-        if session['demo']:
-            pass
-        else:
-            # Ограничение по внешнему ключу FK_SAST_SAS 
-            # не позволяет удалить игровой этап при наличии связанных с ним дочерних команд.
-            try:
-                SAS.get(SAS_ID = sasid).delete_instance()
-            except IntegrityError:
-                flash('Вы не можете удалить этот игровой этап, пока в него добавлена хотя бы одна команда', 'danger')
-
-        return redirect(
-            url_for('listSAS',
-                seasonid = seasonid,
-                ageid    = ageid))
+    # Редирект на вид list
+    return redirect(
+        url_for('listSAS',
+            seasonid = seasonid,
+            ageid    = ageid))
